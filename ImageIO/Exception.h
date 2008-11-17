bc08996208a75083f7380adf888ee20bbdc7125e
Edgar Velazquez-Armendariz <edgarv@users.noreply.github.com>
2008-11-17 03:02:50 +0000

    Subclasses of std::exception to distinguish between exceptions (and make gcc happy).

---
+++ ImageIO/Exception.h
@@ -0,0 +1,42 @@
+#if !defined(PCG_EXCEPTION_H)
+#define PCG_EXCEPTION_H
+
+#include "ImageIO.h"
+#include <string.h>
+
+// Macro to save typing when subclassing the basic exception
+// based on the IexBaseExc.h macro
+#define PCG_DEFINE_EXC(name, base)				            \
+    class name: public base				                    \
+    {							                            \
+      public:                                               \
+	name (const char* text=0)      throw(): base (text) {}	\
+	name (const std::string &text) throw(): base (text) {}	\
+    };
+
+namespace pcg {
+
+	// Basic exception type
+	class IMAGEIO_API Exception: public std::exception
+	{
+	  public:
+		Exception (const char* text=0) throw()     : message(text) {}
+		Exception (const std::string &text) throw(): message(text) {}
+
+		virtual ~Exception() throw () {}
+
+		virtual const char * what () const throw () {
+			return message.c_str();
+		}
+
+	private:
+		std::string message;
+	};
+
+	// Some specific exceptions
+	PCG_DEFINE_EXC(IllegalArgumentException, Exception);
+	PCG_DEFINE_EXC(IOException, Exception);
+
+}
+
+#endif /* PCG_EXCEPTION_H */
