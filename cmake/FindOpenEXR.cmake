fc6f7ff9db4acb4d2d746cd3c71508a78e9eb4ef
Edgar Velazquez-Armendariz <edgarv@users.noreply.github.com>
2011-01-13 02:01:51 -0500

    Improve the FindOpenEXR module. It behaves much closer to the standard Find* modules.

--- cmake/FindOpenEXR.cmake
+++ cmake/FindOpenEXR.cmake
@@ -1,17 +1,43 @@
-# - Find OpenEXR the library
+# - Find OpenEXR
 # Find the native OpenEXR includes and libraries
-# This module defines
-#  OpenEXR_INCLUDE_DIR, where to find OpenEXR/ImfRgbaFile.h, etc.
-#  OpenEXR_LIBRARIES, libraries to link against to use OpenEXR.
-#  OpenEXR_FOUND, If false, do not try to use OpenEXR.
+# This module defines the following read-only variables:
+#  OpenEXR_INCLUDE_DIR - where to find OpenEXR/ImfRgbaFile.h, etc.
+#  OpenEXR_LIBRARIES   - libraries to link against to use OpenEXR.
+#  OPENEXR_FOUND       - if false, do not try to use OpenEXR.
+#
+# These variables alter the behavior of the module when defined before calling
+# find_package(OpenEXR):
+#  OpenEXR_ROOT_DIR - Base location of the OpenEXR installation.
+#
+#=============================================================================
+# Edgar Velázquez-Armendáriz, Cornell University (cs.cornell.edu - eva5)
+# Distributed under the OSI-approved MIT License (the "License")
+# 
+# Copyright (c) 2008-2010 Program of Computer Graphics, Cornell University
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#=============================================================================
 
-
-
-# TODO: For Linux the easiest option should be to use FindPkgConfig !!
-# TODO: This is just a super hack! Implement this properly!
+# TODO: Add support for pkgconfig
 
 find_path(OpenEXR_INCLUDE_DIR ImfRgbaFile.h
-  HINTS ${OPENEXR_ROOT_DIR}
+  HINTS ${OpenEXR_ROOT_DIR}
   PATHS /usr/include /opt/local
   PATH_SUFFIXES OpenEXR
   )
@@ -21,37 +47,28 @@ mark_as_advanced(OpenEXR_INCLUDE_DIR)
 include(FindReleaseAndDebug)
 set(SEARCH_PATH /usr/lib64 /opt/local)
 
-FIND_RELEASE_AND_DEBUG(HALF NAMES Half DEFAULT_SUFFIXES
-  PATHS ${OPENEXR_ROOT_DIR})
-FIND_RELEASE_AND_DEBUG(IEX NAMES Iex DEFAULT_SUFFIXES
-  PATHS ${OPENEXR_ROOT_DIR})
-FIND_RELEASE_AND_DEBUG(IMATH NAMES Imath DEFAULT_SUFFIXES
-  PATHS ${OPENEXR_ROOT_DIR})
-FIND_RELEASE_AND_DEBUG(ILMTHREAD NAMES IlmThread DEFAULT_SUFFIXES
-  PATHS ${OPENEXR_ROOT_DIR})
-FIND_RELEASE_AND_DEBUG(ILMIMF NAMES IlmImf DEFAULT_SUFFIXES
-  PATHS ${OPENEXR_ROOT_DIR})
- 
+FIND_RELEASE_AND_DEBUG(OpenEXR_HALF NAMES Half DEFAULT_SUFFIXES
+  PATHS ${OpenEXR_ROOT_DIR})
+FIND_RELEASE_AND_DEBUG(OpenEXR_IEX NAMES Iex DEFAULT_SUFFIXES
+  PATHS ${OpenEXR_ROOT_DIR})
+FIND_RELEASE_AND_DEBUG(OpenEXR_IMATH NAMES Imath DEFAULT_SUFFIXES
+  PATHS ${OpenEXR_ROOT_DIR})
+FIND_RELEASE_AND_DEBUG(OpenEXR_ILMTHREAD NAMES IlmThread DEFAULT_SUFFIXES
+  PATHS ${OpenEXR_ROOT_DIR})
+FIND_RELEASE_AND_DEBUG(OpenEXR_ILMIMF NAMES IlmImf DEFAULT_SUFFIXES
+  PATHS ${OpenEXR_ROOT_DIR})
 
-# Sets the result
-set(OpenEXR_FOUND FALSE)
-if(OpenEXR_INCLUDE_DIR AND ILMIMF_LIBRARY AND
-   HALF_LIBRARY AND IEX_LIBRARY AND IMATH_LIBRARY AND ILMTHREAD_LIBRARY)
-  set(OpenEXR_FOUND TRUE)
-endif(OpenEXR_INCLUDE_DIR AND ILMIMF_LIBRARY AND
-   HALF_LIBRARY AND IEX_LIBRARY AND IMATH_LIBRARY AND ILMTHREAD_LIBRARY)
-   
-if(OpenEXR_FOUND)
-  set(OpenEXR_LIBRARIES ${HALF_LIBRARY} ${IEX_LIBRARY}
-    ${IMATH_LIBRARY} ${ILMTHREAD_LIBRARY} ${ILMIMF_LIBRARY} )
-  if(NOT OpenEXR_FIND_QUIETLY)
-    message(STATUS "Found OpenEXR.")
-  endif(NOT OpenEXR_FIND_QUIETLY)
-else(OpenEXR_FOUND)
-  set(OpenEXR_PREFIX_PATH "OpenEXR_PREFIX_PATH-NOTFOUND" CACHE PATH 
-    "Unknown OpenEXR base location." FORCE)
-  if(OpenEXR_FIND_REQUIRED)
-    message(FATAL_ERROR "OpenEXR not found!")
-  endif(OpenEXR_FIND_REQUIRED)
-endif(OpenEXR_FOUND)
-   
+
+# Handle the result
+include(FindReleaseAndDebug)
+
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(OpenEXR DEFAULT_MSG OpenEXR_INCLUDE_DIR
+  OpenEXR_HALF_LIBRARY OpenEXR_IEX_LIBRARY OpenEXR_IMATH_LIBRARY
+  OpenEXR_ILMTHREAD_LIBRARY OpenEXR_ILMIMF_LIBRARY)
+
+if (OPENEXR_FOUND)
+  set(OpenEXR_LIBRARIES
+    ${OpenEXR_HALF_LIBRARY} ${OpenEXR_IEX_LIBRARY}
+    ${OpenEXR_IMATH_LIBRARY} ${OpenEXR_ILMTHREAD_LIBRARY}
+    ${OpenEXR_ILMIMF_LIBRARY})
+endif()
