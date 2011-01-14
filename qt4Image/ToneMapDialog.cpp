92a626600a82e0075e06475d52f2d67fff2c30e5
Edgar Velazquez-Armendariz <edgarv@users.noreply.github.com>
2011-01-14 17:52:27 -0500

    Initial implementation of the tonemapping dialog

---
+++ qt4Image/ToneMapDialog.cpp
@@ -0,0 +1,74 @@
+#include "ToneMapDialog.h"
+#include <QDebug>
+
+// Static array with the label for the zones. The last one is just to avoid
+// clamping for the very last value
+namespace
+{
+const QString zones_txt[] = {
+    QObject::tr("Zone 0"),
+    QObject::tr("Zone I"),
+    QObject::tr("Zone II"),
+    QObject::tr("Zone III"),
+    QObject::tr("Zone IV"),
+    QObject::tr("Zone V"),
+    QObject::tr("Zone VI"),
+    QObject::tr("Zone VII"),
+    QObject::tr("Zone VIII"),
+    QObject::tr("Zone IX"),
+    QObject::tr("Zone X"),
+    QObject::tr("Zone X")
+};
+} // namespace
+
+
+
+ToneMapDialog::ToneMapDialog(QWidget *parent) :
+QDialog(parent, Qt::Dialog),
+whitePointValidator(0.0, 1.0, 16, this), keyValidator(0.0, 1.0, 16, this),
+l_min(0.0f), l_max(1.0f)
+{
+    // Setup things as in the designer
+    setupUi(this);
+
+    // Align some labels
+    keyLbl->setMinimumSize(whitePointLbl->sizeHint());
+    darkLbl->setMinimumWidth(minLbl->sizeHint().width());
+    brightLbl->setMinimumWidth(maxLbl->sizeHint().width());
+
+    // Connect the slider's signals
+    connect ( whitePointSldr, SIGNAL(valueChanged(int)), 
+              this,           SLOT(whitePointSliderChanged(int)) );
+    connect ( keySldr, SIGNAL(valueChanged(int)), 
+              this,    SLOT(keySliderChanged(int)) );
+
+    // Configure the validators
+    whitePointTxt->setValidator(&whitePointValidator);
+    keyTxt->setValidator(&keyValidator);
+}
+
+void ToneMapDialog::updateData(float whitePoint, float key,
+                               float minimum, float maximum)
+{
+    // Don't allow to enable if the values are sense-less
+    this->reinhard02Chk->setEnabled(!(minimum > maximum ||
+        qFuzzyCompare(minimum, maximum) || whitePoint < minimum));
+
+    // Set the new values and ranges
+    whitePointTxt->setText(QString::number(whitePoint));
+    keyTxt->setText(QString::number(key));
+    l_min = minimum;
+    l_max = 2.0f * qMax(maximum, whitePoint);
+    qDebug() << "white: " << whitePoint << " min: " << l_min << " max: " << l_max;
+}
+
+void ToneMapDialog::keySliderChanged(int rawValue)
+{
+    qDebug() << "Key value: " << rawValue << " zone: " << (rawValue >> 3);
+}
+
+void ToneMapDialog::whitePointSliderChanged(int rawValue)
+{
+    qDebug() << "White point: " << rawValue;
+
+}
---
+++ qt4Image/ToneMapDialog.h
@@ -0,0 +1,55 @@
+// This is the implementation of the actual QWidget which
+// will contain the main window of the application
+//
+#if !defined(PCG_TONEMAPDIALOG_H)
+#define PCG_TONEMAPDIALOG_H
+
+#include <ui_tonemap.h>
+
+#include <QDoubleValidator>
+
+
+// Define the window form using the multiple inheritance approach
+class ToneMapDialog : public QDialog, public Ui::ToneMapDialog
+{
+    // We need this macro to process signals and slots
+    Q_OBJECT
+
+public:
+    /// Transfer object used to set up the dialog
+    struct Data {
+        float whitePoint;
+        float key;
+        float minimum;
+        float maximum;
+    };
+
+    // Basic constructor, it initializes all the elements of the window
+    ToneMapDialog(QWidget *parent = 0);
+
+    // Update the GUI values
+    void updateData(float whitePoint, float key, float minimum, float maximum);
+
+signals:
+    // Emmit the request for a new white point
+    void whitePointUpdate(float value);
+
+    // Emmit the new key value
+    void keyUpdate(float value);
+
+private slots:
+    // React to the key slider
+    void keySliderChanged(int rawValue);
+
+    // React to the white point slider
+    void whitePointSliderChanged(int rawValue);
+
+private:
+    QDoubleValidator whitePointValidator;
+    QDoubleValidator keyValidator;
+    float l_min;
+    float l_max;
+};
+
+
+#endif /* PCG_TONEMAPDIALOG_H */
