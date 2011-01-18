3e0b5260cf7b72f46b47c6098df885d3d87f9417
Edgar Velazquez-Armendariz <edgarv@users.noreply.github.com>
2011-01-18 15:13:52 -0500

    Added helper object to coordinate a slider and a line-edit

---
+++ qt4Image/QInterpolator.cpp
@@ -0,0 +1,112 @@
+#include "QInterpolator.h"
+#include <QDebug>
+
+QInterpolator::QInterpolator(double minimum, double maximum,
+                             QAbstractSlider *slider, QLineEdit *edit,
+                             QObject *parent):
+QObject(parent),
+m_slider(slider), m_edit(edit), m_validator(minimum, maximum, 16, this)
+{
+    Q_ASSERT(!m_slider.isNull());
+    Q_ASSERT(!m_edit.isNull());
+    Q_ASSERT(minimum < maximum);
+    m_edit->setValidator(&m_validator);
+    setRange(minimum, maximum);
+
+    // Connect the signals
+    connect ( m_slider, SIGNAL(rangeChanged(int,int)),
+        this, SLOT(sliderRangeChanged(int,int)) );
+    connect ( m_slider, SIGNAL(valueChanged(int)), 
+        this, SLOT(sliderChanged(int)) );
+    connect ( m_edit, SIGNAL(editingFinished(void)),
+        this, SLOT(textEdited(void)) );
+}
+
+
+const void QInterpolator::setRange(double minimum, double maximum)
+{
+    Q_ASSERT(minimum < maximum);
+    m_validator.setRange(minimum, maximum, m_validator.decimals());
+    updateState(minimum, maximum, m_slider->minimum(), m_slider->maximum());
+}
+
+
+
+void QInterpolator::sliderRangeChanged(int minimum, int maximum)
+{
+    updateState(bottom(), top(), minimum, maximum);
+}
+
+
+void QInterpolator::sliderChanged(int sliderValue)
+{
+    const double value = toValue(sliderValue);
+    Q_ASSERT(bottom() <= value && value <= top());
+    QString text = QString::number(value);
+    int position;
+    QValidator::State state = m_validator.validate(text, position);
+    if (state != QValidator::Acceptable) {
+        m_validator.fixup(text);
+    }
+    state = m_validator.validate(text, position);
+    Q_ASSERT(state == QValidator::Acceptable);
+    const bool blocked = m_edit->blockSignals(true);
+    m_edit->setText(text);
+    m_edit->blockSignals(blocked);
+
+    emit valueChanged(value);
+}
+
+
+void QInterpolator::textEdited()
+{
+    bool ok;
+    const double value = m_edit->text().toDouble(&ok);
+    if (!ok) return;
+    const int pos = toSliderValue(value);
+    const bool blocked = m_slider->blockSignals(true);
+    Q_ASSERT(m_slider->minimum() <= pos && pos <= m_slider->maximum());
+    m_slider->setValue(pos);
+    m_slider->blockSignals(blocked);
+
+    emit valueChanged(value);
+}
+
+
+
+QLinearInterpolator::QLinearInterpolator(double minimum, double maximum,
+                                         QAbstractSlider *slider,
+                                         QLineEdit *edit, QObject *parent)
+: QInterpolator(minimum, maximum, slider, edit, parent),
+  m_slope(0.0), m_slopeInv(0.0), m_intercept(0.0)
+{
+    // C++ doesn't like virtual functions inside constructors
+    updateState(minimum, maximum, slider->minimum(), slider->maximum());
+}
+
+
+void QLinearInterpolator::updateState(double minimum, double maximum,
+                                      int sliderMinimum, int sliderMaximum)
+{
+    m_slope = (maximum - minimum) / 
+        static_cast<double>(sliderMaximum - sliderMinimum);
+    m_slopeInv = 1.0/m_slope;
+    m_intercept = minimum - m_slope*static_cast<double>(sliderMinimum);
+}
+
+
+int QLinearInterpolator::toSliderValue(double value)
+{
+    Q_ASSERT(m_slopeInv != 0.0);
+    int pos = qRound((value - bottom())*m_slopeInv + 
+        static_cast<double>(sliderMinimum()));
+    return pos;
+}
+
+
+double QLinearInterpolator::toValue(int sliderValue)
+{
+    Q_ASSERT(m_slope != 0.0);
+    double value = m_slope*static_cast<double>(sliderValue) + m_intercept;
+    return value;
+}
---
+++ qt4Image/QInterpolator.h
@@ -0,0 +1,97 @@
+#if !defined(PCG_QINTERPOLATOR)
+#define PCG_QINTERPOLATOR
+
+#include <QAbstractSlider>
+#include <QLineEdit>
+#include <QPointer>
+#include "QFixupDoubleValidator.h"
+
+// Class responsible to map a range of values between a line-edit and a slider
+// with validation while keeping both controls synchronized. It also emmits a
+// signal when the value is changed.
+class QInterpolator : public QObject
+{
+    Q_OBJECT
+
+public:
+    QInterpolator(double minimum, double maximum,
+        QAbstractSlider *slider, QLineEdit *edit, QObject *parent = 0);
+
+    const void setRange(double minimum, double maximum);
+
+
+protected:
+
+    // To be implemented by the subclasses:
+
+    // Function called to update the internal state. The default
+    // implementation doesn't do anything!
+    virtual void updateState(double minimum, double maximum,
+        int sliderMinimum, int sliderMaximum)
+    {
+    }
+
+    // Given a certain value (within the valid range), calculate the
+    // appropriate value it maps to into the slider
+    virtual int toSliderValue(double value) = 0;
+
+    // Given a certain slider value (within the valid range), calculate
+    // the value it maps to
+    virtual double toValue(int sliderValue) = 0;
+
+    inline const double top() {
+        return m_validator.top();
+    }
+
+    inline const double bottom() {
+        return m_validator.bottom();
+    }
+
+    inline const int sliderMinimum() {
+        return !m_slider.isNull() ? m_slider->minimum() : -1;
+    }
+
+    inline const int sliderMaximum() {
+        return !m_slider.isNull() ? m_slider->maximum() : -1;
+    }
+
+
+signals:
+    void valueChanged(double value);
+    
+
+private slots:
+    void sliderRangeChanged(int minimum, int maximum);
+    void sliderChanged(int sliderValue);
+    void textEdited();
+
+private:
+    QPointer<QAbstractSlider> m_slider;
+    QPointer<QLineEdit> m_edit;
+    QFixupDoubleValidator m_validator;
+};
+
+
+
+// Concrete implementation which implements simple linear interpolation
+class QLinearInterpolator : public QInterpolator
+{
+    Q_OBJECT
+
+public:
+    QLinearInterpolator(double minimum, double maximum,
+        QAbstractSlider *slider, QLineEdit *edit, QObject *parent = 0);
+
+protected:
+    virtual void updateState(double minimum, double maximum,
+        int sliderMinimum, int sliderMaximum);
+    virtual int toSliderValue(double value);
+    virtual double toValue(int sliderValue);
+
+private:
+    double m_slope;
+    double m_slopeInv;
+    double m_intercept;
+};
+
+#endif // PCG_QINTERPOLATOR
