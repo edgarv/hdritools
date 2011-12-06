43fa5a61519ec014809fb119bc5f37120530c906
Edgar Velazquez-Armendariz <edgarv@users.noreply.github.com>
2011-12-05 18:12:31 -0800

    Untabify exrinfo.cpp

--- OpenEXR_Matlab/exrinfo.cpp
+++ OpenEXR_Matlab/exrinfo.cpp
@@ -44,18 +44,18 @@ namespace
 // Temporaty struct to hold a name and a matlab value
 struct Pair
 {
-	const char * name;
-	mxArray * value;
+    const char * name;
+    mxArray * value;
 
-	Pair() : name(NULL), value(NULL) {}
+    Pair() : name(NULL), value(NULL) {}
 
-	Pair(const char * pName, mxArray * pValue = NULL) :
-	name(pName), value(pValue)
-	{}
+    Pair(const char * pName, mxArray * pValue = NULL) :
+    name(pName), value(pValue)
+    {}
 
-	bool isValid() const {
-		return name != NULL && value != NULL;
-	}
+    bool isValid() const {
+        return name != NULL && value != NULL;
+    }
 };
 
 
@@ -63,21 +63,21 @@ struct Pair
 // Create a cell array with only the name of the channels
 mxArray * getChannelNames(const ChannelList & channels)
 {
-	typedef ChannelList::ConstIterator ChannelIterator;
+    typedef ChannelList::ConstIterator ChannelIterator;
 
-	std::vector<mxArray *> channelNames;
-	for (ChannelIterator it = channels.begin(); it != channels.end(); ++it)
-	{
-		mxArray * mStr = mxCreateString(it.name());
-		channelNames.push_back(mStr);
-	}
-	assert(!channelNames.empty());
+    std::vector<mxArray *> channelNames;
+    for (ChannelIterator it = channels.begin(); it != channels.end(); ++it)
+    {
+        mxArray * mStr = mxCreateString(it.name());
+        channelNames.push_back(mStr);
+    }
+    assert(!channelNames.empty());
 
-	mxArray * cells = mxCreateCellMatrix(1, channelNames.size());
-	for (size_t i = 0; i != channelNames.size(); ++i) {
-		mxSetCell(cells, i, channelNames[i]);
-	}
-	return cells;
+    mxArray * cells = mxCreateCellMatrix(1, channelNames.size());
+    for (size_t i = 0; i != channelNames.size(); ++i) {
+        mxSetCell(cells, i, channelNames[i]);
+    }
+    return cells;
 }
 
 
@@ -85,41 +85,41 @@ mxArray * getChannelNames(const ChannelList & channels)
 // Create and populate a containters.Map object with the attributes
 mxArray * getAttributesMap(const Header & header)
 {
-	std::vector<Pair> attributes;
+    std::vector<Pair> attributes;
 
-	// Get the full set of attributes
-	for (Header::ConstIterator it = header.begin(); it != header.end(); ++it) {
-		const Attribute & attr = it.attribute();
-		if (!Attribute::knownType(attr.typeName())) {
-			continue;
-		}
+    // Get the full set of attributes
+    for (Header::ConstIterator it = header.begin(); it != header.end(); ++it) {
+        const Attribute & attr = it.attribute();
+        if (!Attribute::knownType(attr.typeName())) {
+            continue;
+        }
 
-		Pair pair(it.name());
-		pair.value = toMatlab(attr);
-		if (pair.isValid()) {
-			attributes.push_back(pair);
-		}
-	}
-	assert(!attributes.empty());
+        Pair pair(it.name());
+        pair.value = toMatlab(attr);
+        if (pair.isValid()) {
+            attributes.push_back(pair);
+        }
+    }
+    assert(!attributes.empty());
 
-	// Create cell arrays with the attributes' names and values
-	mxArray * nameCell  = mxCreateCellMatrix(1, attributes.size());
-	mxArray * valueCell = mxCreateCellMatrix(1, attributes.size());
-	for (size_t i = 0; i != attributes.size(); ++i) {
-		mxSetCell(nameCell,  i, mxCreateString(attributes[i].name));
-		mxSetCell(valueCell, i, attributes[i].value);
-	}
+    // Create cell arrays with the attributes' names and values
+    mxArray * nameCell  = mxCreateCellMatrix(1, attributes.size());
+    mxArray * valueCell = mxCreateCellMatrix(1, attributes.size());
+    for (size_t i = 0; i != attributes.size(); ++i) {
+        mxSetCell(nameCell,  i, mxCreateString(attributes[i].name));
+        mxSetCell(valueCell, i, attributes[i].value);
+    }
 
-	// Create the attributes map
-	mxArray * mapHandle = NULL;
-	mxArray * mapArgs[2] = {nameCell, valueCell};
-	if (mexCallMATLAB(1, &mapHandle, 2, &mapArgs[0], "containers.Map") != 0) {
-		mexErrMsgIdAndTxt("OpenEXR:exception",
-			"Could not create the attribute map.");
-		return NULL;
-	}
+    // Create the attributes map
+    mxArray * mapHandle = NULL;
+    mxArray * mapArgs[2] = {nameCell, valueCell};
+    if (mexCallMATLAB(1, &mapHandle, 2, &mapArgs[0], "containers.Map") != 0) {
+        mexErrMsgIdAndTxt("OpenEXR:exception",
+            "Could not create the attribute map.");
+        return NULL;
+    }
 
-	return mapHandle;
+    return mapHandle;
 }
 
 
@@ -145,38 +145,38 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
     mxFree(inputfilePtr); inputfilePtr = static_cast<char*>(0);
     
     try {
-		// Open a file, but only read the header
-		Header header;
-		{
-			StdIFStream is(inputfile.c_str());
-			int version = 0;
-			header.readFrom(is, version);
-		}
+        // Open a file, but only read the header
+        Header header;
+        {
+            StdIFStream is(inputfile.c_str());
+            int version = 0;
+            header.readFrom(is, version);
+        }
 
-		const Box2i & dw = header.dataWindow();
-		const int width  = dw.max.x - dw.min.x + 1;
-		const int height = dw.max.y - dw.min.y + 1;
+        const Box2i & dw = header.dataWindow();
+        const int width  = dw.max.x - dw.min.x + 1;
+        const int height = dw.max.y - dw.min.y + 1;
 
-		// List of channel names
-		mxArray * channelNames = getChannelNames(header.channels());
+        // List of channel names
+        mxArray * channelNames = getChannelNames(header.channels());
 
-		// Attributes map
-		mxArray * attributesMap = getAttributesMap(header);
+        // Attributes map
+        mxArray * attributesMap = getAttributesMap(header);
 
-		// Size in matlab style (rows by columns)
-		const int dataSize[] = {height, width};
-		mxArray * size = fromArray(dataSize);
+        // Size in matlab style (rows by columns)
+        const int dataSize[] = {height, width};
+        mxArray * size = fromArray(dataSize);
 
-		// Build the structure
-		const char* fields[] = {"channels", "size", "attributes"};
-		mxArray *bStruct = mxCreateStructMatrix(1, 1,
-			sizeof(fields)/sizeof(const char *), &fields[0]);
-		mxSetField(bStruct, 0, "channels",   channelNames);
-		mxSetField(bStruct, 0, "size",       size);
-		mxSetField(bStruct, 0, "attributes", attributesMap);
-		
-		// Assign the result
-		plhs[0] = bStruct;
+        // Build the structure
+        const char* fields[] = {"channels", "size", "attributes"};
+        mxArray *bStruct = mxCreateStructMatrix(1, 1,
+            sizeof(fields)/sizeof(const char *), &fields[0]);
+        mxSetField(bStruct, 0, "channels",   channelNames);
+        mxSetField(bStruct, 0, "size",       size);
+        mxSetField(bStruct, 0, "attributes", attributesMap);
+        
+        // Assign the result
+        plhs[0] = bStruct;
     }
     catch( std::exception &e ) {
         mexErrMsgIdAndTxt("OpenEXR:exception", e.what());
