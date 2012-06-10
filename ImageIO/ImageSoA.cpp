a6180dde42c6e76735273d42bfffc5dd95995900
Edgar Velazquez-Armendariz <edgarv@users.noreply.github.com>
2012-06-10 01:11:18 -0400

    Parallel version of the copy constructor for Image<Rgba32F>
    The new code is much, much larger than the trivial, serial approach for a
    reduction of about 50% in runtime (seems almost not worth the hassle)

--- ImageIO/ImageSoA.cpp
+++ ImageIO/ImageSoA.cpp
@@ -17,59 +17,102 @@
 #include "StdAfx.h"
 #include "Rgba32F.h"
 
+#include <tbb/blocked_range.h>
+#include <tbb/parallel_for.h>
+
+
+namespace
+{
+struct CopyFunctor
+{
+    typedef tbb::blocked_range<int> Range;
+
+    struct Args
+    {
+        const pcg::Image<pcg::Rgba32F, pcg::TopDown> &src;
+        pcg::RGBImageSoA &dest;
+
+        Args(const pcg::Image<pcg::Rgba32F, pcg::TopDown> &source,
+            pcg::RGBImageSoA &target) : src(source), dest(target)
+        {}
+    };
+
+
+    CopyFunctor(const Args &args) : m_args(args) {}
+    CopyFunctor(const CopyFunctor &other) : m_args(other.m_args) {}
+
+
+    inline void
+    copy1(float& r, float& g, float& b, const pcg::Rgba32F& src) const
+    {
+        r = src.r();
+        g = src.g();
+        b = src.b();
+    }
+
+    inline void
+    copy4(float* r, float* g, float *b, const pcg::Rgba32F* src, int off) const
+    {
+        __m128 p0 = src[off];
+        __m128 p1 = src[off + 1];
+        __m128 p2 = src[off + 2];
+        __m128 p3 = src[off + 3];
+        PCG_MM_TRANSPOSE4_PS (p0, p1, p2, p3);
+
+        // FIXME Is it OK to ignore alpha?
+        _mm_stream_ps(r + off, p3);
+        _mm_stream_ps(g + off, p2);
+        _mm_stream_ps(b + off, p1);
+    }
+
+    void operator() (Range& range) const
+    {
+        assert(range.begin() >= 0);
+        assert(range.end() <= m_args.src.Size());
+        assert(range.begin() <= range.end());
+
+        // The range might not be in appropriate multiples of four
+        const int beginSSE = (range.begin() + 3) & ~0x3;
+        const int endSSE   = range.end() & ~0x3;
+        assert((endSSE - beginSSE) % 4 == 0);
+        assert((beginSSE-range.begin())+(endSSE-beginSSE)+(range.end()-endSSE)\
+            ==range.size());
+
+        float * r = m_args.dest.GetDataPointer<pcg::RGBImageSoA::R>();
+        float * g = m_args.dest.GetDataPointer<pcg::RGBImageSoA::G>();
+        float * b = m_args.dest.GetDataPointer<pcg::RGBImageSoA::B>();
+        const pcg::Rgba32F* src = m_args.src.GetDataPointer();
+
+        // Copy individual items until the first multiple of 4, so that they
+        // are aligned to 16 bytes (1 to 3 elements)
+        for (int i = range.begin(); i < beginSSE; ++i) {
+            copy1(r[i], g[i], b[i], src[i]);
+        }
+
+        // Copy all the blocks of 4 pixels. This is the heaviest loop
+        for (int offset = beginSSE; offset < endSSE; offset += 4)
+        {
+            copy4(r, g, b, src, offset);
+        }
+
+        // Copy any remaining elements (1 to 3)
+        for (int i = endSSE; i < range.end(); ++i) {
+            copy1(r[i], g[i], b[i], src[i]);
+        }
+    }
+
+private:
+    Args m_args;
+};
+
+} // Namespace
+
 
 
 void
 pcg::RGBImageSoA::copyImage(const pcg::Image<pcg::Rgba32F, pcg::TopDown> &img)
 {
-    // Copy four elements at the time
-    const int size      = img.Size();
-    const int size4     = size >> 2;
-    const int sizeExtra = size &  0x3;
-
-    float * r = GetDataPointer<R>();
-    float * g = GetDataPointer<G>();
-    float * b = GetDataPointer<B>();
-    const pcg::Rgba32F* src = img.GetDataPointer();
-
-    for (int idx = 0; idx < size4; ++idx) {
-        __m128 p0 = src[0];
-        __m128 p1 = src[1];
-        __m128 p2 = src[2];
-        __m128 p3 = src[3];
-        PCG_MM_TRANSPOSE4_PS (p0, p1, p2, p3);
-
-        // FIXME Is it OK to ignore alpha?
-        _mm_stream_ps(r, p3);
-        _mm_stream_ps(g, p2);
-        _mm_stream_ps(b, p1);
-
-        // Advance all pointers
-        r   += 4;
-        g   += 4;
-        b   += 4;
-        src += 4;
-    }
-
-    // Copy for the remaining elements, zeroing the padding
-    if (sizeExtra != 0) {
-        __m128 values[4];
-        for (int idx = 0; idx < sizeExtra; ++idx) {
-            values[idx] = src[idx];
-        }
-        for (int idx = sizeExtra; idx < 4; ++idx) {
-            values[idx] = _mm_setzero_ps();
-        }
-
-        __m128 p0 = values[0];
-        __m128 p1 = values[1];
-        __m128 p2 = values[2];
-        __m128 p3 = values[3];
-        PCG_MM_TRANSPOSE4_PS (p0, p1, p2, p3);
-
-        // FIXME Is it OK to ignore alpha?
-        _mm_stream_ps(r, p3);
-        _mm_stream_ps(g, p2);
-        _mm_stream_ps(b, p1);
-    }
+    CopyFunctor::Range range(0, img.Size(), 4);
+    CopyFunctor functor(CopyFunctor::Args(img, *this));
+    tbb::parallel_for(range, functor);
 }
