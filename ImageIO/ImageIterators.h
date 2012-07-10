55c3262eaf9ab034da2f1b6487f17b65fcd0ff94
Edgar Velazquez-Armendariz <edgarv@users.noreply.github.com>
2012-07-10 16:24:27 -0400

    Template image iterators, in preparation for supporting larger vectors for AVX

--- ImageIO/ImageIterators.h
+++ ImageIO/ImageIterators.h
@@ -73,104 +73,96 @@ struct RGBA32FVec4
 
 
 
-// Helper struct which represent 4 RGBA values, in SoA fashion which are backed
-// by other data
-struct RGBA32FVec4Ref
+// Template to represent SoA-styled RGBA values, backed by external data
+template <class VectorType>
+struct RGBAVecRef
 {
 private:
-    __m128& m_r;
-    __m128& m_g;
-    __m128& m_b;
-    __m128& m_a;
+    VectorType& m_r;
+    VectorType& m_g;
+    VectorType& m_b;
+    VectorType& m_a;
 
 public:
-    RGBA32FVec4Ref(__m128& rRef, __m128& gRef, __m128& bRef, __m128& aRef) :
+    RGBAVecRef(VectorType& rRef, VectorType& gRef,
+               VectorType& bRef, VectorType& aRef) :
     m_r(rRef), m_g(gRef), m_b(bRef), m_a(aRef)
     {}
 
-    operator RGBA32FVec4() const {
-        RGBA32FVec4 v;
-        v.r() = r();
-        v.g() = g();
-        v.b() = b();
-        v.a() = a();
-        return v;
-    }
-
-    inline __m128& r() {
+    inline VectorType& r() {
         return m_r;
     }
 
-    inline const __m128& r() const {
+    inline const VectorType& r() const {
         return m_r;
     }
 
-    inline __m128& g() {
+    inline VectorType& g() {
         return m_g;
     }
 
-    inline const __m128& g() const {
+    inline const VectorType& g() const {
         return m_g;
     }
 
-    inline __m128& b() {
+    inline VectorType& b() {
         return m_b;
     }
 
-    inline const __m128& b() const {
+    inline const VectorType& b() const {
         return m_b;
     }
 
-    inline __m128& a() {
+    inline VectorType& a() {
         return m_a;
     }
 
-    inline const __m128& a() const {
+    inline const VectorType& a() const {
         return m_a;
     }
 };
 
-// Helper struct which represent 4 RGBA values, in SoA fashion which are backed
-// by other data. This version only allows reads
-struct RGBA32FVec4ConstRef
+
+
+// Template which represent vectors of RGBA values, in SoA fashion,
+// which are backed by other data. This version only allows reads.
+template <class VectorType>
+class RGBAVecConstRef
 {
 private:
-    const __m128& m_r;
-    const __m128& m_g;
-    const __m128& m_b;
-    const __m128& m_a;
+    const VectorType& m_r;
+    const VectorType& m_g;
+    const VectorType& m_b;
+    const VectorType& m_a;
 
 public:
-    RGBA32FVec4ConstRef(const __m128& rRef, const __m128& gRef,
-        const __m128& bRef, const __m128& aRef) :
+    RGBAVecConstRef(const VectorType& rRef, const VectorType& gRef,
+                    const VectorType& bRef, const VectorType& aRef) :
     m_r(rRef), m_g(gRef), m_b(bRef), m_a(aRef)
     {}
 
-    operator RGBA32FVec4() const {
-        RGBA32FVec4 v;
-        v.r() = r();
-        v.g() = g();
-        v.b() = b();
-        v.a() = a();
-        return v;
-    }
-
-    inline const __m128& r() const {
+    inline const VectorType& r() const {
         return m_r;
     }
 
-    inline const __m128& g() const {
+    inline const VectorType& g() const {
         return m_g;
     }
 
-    inline const __m128& b() const {
+    inline const VectorType& b() const {
         return m_b;
     }
 
-    inline const __m128& a() const {
+    inline const VectorType& a() const {
         return m_a;
     }
 };
+
+// Helper struct which represent 4 RGBA values
+typedef RGBAVecRef<__m128> RGBA32FVec4Ref;
+
+// Helper struct to represent 4 const RGBA values
+typedef RGBAVecConstRef<__m128> RGBA32FVec4ConstRef;
 
 
 
@@ -386,165 +378,184 @@ struct PixelBGRA8Vec4
 
 
 
-// RGBA SoA Pixel Iterator concept for SoA images. It iterates
-// the image in groups of 4 pixels, returning a fresh RGBA32FVec4 with the
-// next 4 R,G,B,A values in SoA form. Thus this is only a "read-only" iterator.
+// Helper traits to find appropriate classes for the iterator
+template <int N>
+struct RGBA32FVec_traits;
+
+template <>
+struct RGBA32FVec_traits<4>
+{
+    typedef RGBA32FVec4Ref VecRef;
+    typedef RGBA32FVec4ConstRef VecConstRef;
+    typedef __m128* pointer_type;
+};
+
+
+
+// RGBA SoA Pixel Iterator concept template for SoA images. It iterates
+// the image in groups of N pixels, returning a RGBAVecConstRef<N> with the
+// next N R,G,B,A values in SoA form. Thus this is a "read-only" iterator.
 // The current implementation only iterates in the original scanlie order of
 // an image.
-class RGBA32FVec4ImageSoAIterator :
-public std::iterator<std::random_access_iterator_tag, RGBA32FVec4ConstRef>
+template <int N>
+class RGBA32FVecImageSoAIterator :
+public std::iterator<std::random_access_iterator_tag,
+                    typename RGBA32FVec_traits<N>::VecConstRef>
 {
 public:
 
     // Default constructor, which creates an invalid iterator
-    RGBA32FVec4ImageSoAIterator() :
+    RGBA32FVecImageSoAIterator() :
     m_r(NULL), m_g(NULL), m_b(NULL), m_a(NULL), m_offset(0)
     {}
 
     // Copy constructor
-    RGBA32FVec4ImageSoAIterator(const RGBA32FVec4ImageSoAIterator& other) :
+    RGBA32FVecImageSoAIterator(const RGBA32FVecImageSoAIterator& other) :
     m_r(other.m_r), m_g(other.m_g), m_b(other.m_b), m_a(other.m_a),
     m_offset(other.m_offset)
     {}
 
     // Equality/inequality comparisons using only the offsets
 
-    inline bool operator== (const RGBA32FVec4ImageSoAIterator& other) const {
+    inline bool operator== (const RGBA32FVecImageSoAIterator& other) const {
         assert(haveSameBase(other));
         return m_offset == other.m_offset;
     }
-    inline bool operator!= (const RGBA32FVec4ImageSoAIterator& other) const {
+    inline bool operator!= (const RGBA32FVecImageSoAIterator& other) const {
         assert(haveSameBase(other));
         return m_offset != other.m_offset;
     }
 
     // Inequality comparisons between iterators
 
-    inline bool operator< (const RGBA32FVec4ImageSoAIterator& other) const {
+    inline bool operator< (const RGBA32FVecImageSoAIterator& other) const {
         assert(haveSameBase(other));
         return m_offset < other.m_offset;
     }
-    inline bool operator> (const RGBA32FVec4ImageSoAIterator& other) const {
+    inline bool operator> (const RGBA32FVecImageSoAIterator& other) const {
         assert(haveSameBase(other));
         return m_offset > other.m_offset;
     }
-    inline bool operator<= (const RGBA32FVec4ImageSoAIterator& other) const {
+    inline bool operator<= (const RGBA32FVecImageSoAIterator& other) const {
         assert(haveSameBase(other));
         return m_offset <= other.m_offset;
     }
-    inline bool operator>= (const RGBA32FVec4ImageSoAIterator& other) const {
+    inline bool operator>= (const RGBA32FVecImageSoAIterator& other) const {
         assert(haveSameBase(other));
         return m_offset >= other.m_offset;
     }
 
     // Increments
 
-    inline RGBA32FVec4ImageSoAIterator& operator++() {
+    inline RGBA32FVecImageSoAIterator& operator++() {
         ++m_offset;
         return *this;
     }
-    inline RGBA32FVec4ImageSoAIterator& operator++(int) {
+    inline RGBA32FVecImageSoAIterator& operator++(int) {
         ++m_offset;
         return *this;
     }
 
     // Decrements
 
-    inline RGBA32FVec4ImageSoAIterator& operator--() {
+    inline RGBA32FVecImageSoAIterator& operator--() {
         --m_offset;
         return *this;
     }
 
-    inline RGBA32FVec4ImageSoAIterator& operator--(int) {
+    inline RGBA32FVecImageSoAIterator& operator--(int) {
         --m_offset;
         return *this;
     }
 
     // Binary arithmetic operators
 
-    inline friend RGBA32FVec4ImageSoAIterator operator + (
-        const RGBA32FVec4ImageSoAIterator& a, difference_type offset)
+    inline friend RGBA32FVecImageSoAIterator operator + (
+        const RGBA32FVecImageSoAIterator& a,
+        typename RGBA32FVecImageSoAIterator<N>::difference_type offset)
     {
-        RGBA32FVec4ImageSoAIterator it(a);
+        RGBA32FVecImageSoAIterator it(a);
         it.m_offset += offset;
         return it;
     }
 
-    inline friend RGBA32FVec4ImageSoAIterator operator + (
-        difference_type offset, const RGBA32FVec4ImageSoAIterator& a)
+    inline friend RGBA32FVecImageSoAIterator operator + (
+        typename RGBA32FVecImageSoAIterator<N>::difference_type offset,
+        const RGBA32FVecImageSoAIterator& a)
     {
-        RGBA32FVec4ImageSoAIterator it(a);
+        RGBA32FVecImageSoAIterator it(a);
         it.m_offset += offset;
         return it;
     }
 
-    inline friend RGBA32FVec4ImageSoAIterator operator - (
-        const RGBA32FVec4ImageSoAIterator& a, difference_type offset)
+    inline friend RGBA32FVecImageSoAIterator operator- (
+        const RGBA32FVecImageSoAIterator& a,
+        typename RGBA32FVecImageSoAIterator<N>::difference_type offset)
     {
-        RGBA32FVec4ImageSoAIterator it(a);
+        RGBA32FVecImageSoAIterator it(a);
         it.m_offset -= offset;
         return it;
     }
 
-    inline friend difference_type operator- (
-        const RGBA32FVec4ImageSoAIterator& a,
-        const RGBA32FVec4ImageSoAIterator& b)
+    inline friend typename RGBA32FVecImageSoAIterator<N>::difference_type
+        operator- (const RGBA32FVecImageSoAIterator& a,
+                   const RGBA32FVecImageSoAIterator& b)
     {
         assert(a.haveSameBase(b));
-        RGBA32FVec4ImageIterator::difference_type d = a.m_offset - b.m_offset;
+        typename RGBA32FVecImageSoAIterator<N>::difference_type d =
+            a.m_offset - b.m_offset;
         return d;
     }
 
     // Compound assignment
 
-    inline RGBA32FVec4ImageSoAIterator& operator+=(difference_type offset) {
+    inline RGBA32FVecImageSoAIterator& operator+=(
+        typename RGBA32FVecImageSoAIterator<N>::difference_type offset)
+    {
         m_offset += offset;
         return *this;
     }
 
-    inline RGBA32FVec4ImageSoAIterator& operator-=(difference_type offset) {
+    inline RGBA32FVecImageSoAIterator& operator-=(
+        typename RGBA32FVecImageSoAIterator<N>::difference_type offset)
+    {
         m_offset -= offset;
         return *this;
     }
 
     // Offset dereference
 
-    // Be aware that this returns a special temporary element!
-    inline RGBA32FVec4Ref operator[] (size_t idx)
+    inline typename RGBA32FVec_traits<N>::VecConstRef
+        operator[] (size_t idx) const
     {
+        typedef typename RGBA32FVec_traits<N>::VecConstRef V;
         const size_t offset = m_offset + idx;
-        RGBA32FVec4Ref p(*(m_r + offset), *(m_g + offset),
-                         *(m_b + offset), *(m_a + offset));
-        return p;
-    }
-
-    // Be aware that this returns a special const temporary element!
-    inline RGBA32FVec4ConstRef operator[] (size_t idx) const
-    {
-        const size_t offset = m_offset + idx;
-        RGBA32FVec4ConstRef p(*(m_r + offset), *(m_g + offset),
-                              *(m_b + offset), *(m_a + offset));
+        V p(*(m_r + offset), *(m_g + offset),
+            *(m_b + offset), *(m_a + offset));
         return p;
     }
 
     // Notice that this supports only read-only access
-    inline RGBA32FVec4ConstRef operator*() const
+    inline typename RGBA32FVec_traits<N>::VecConstRef
+        operator*() const
     {
-        RGBA32FVec4ConstRef p(*(m_r + m_offset), *(m_g + m_offset),
-                              *(m_b + m_offset), *(m_a + m_offset));
+        typedef typename RGBA32FVec_traits<N>::VecConstRef V;
+        V p(*(m_r + m_offset), *(m_g + m_offset),
+            *(m_b + m_offset), *(m_a + m_offset));
         return p;
     }
 
 
     // Create an iterator at the beginning of the image, moving in the same
     // direction as the established scanline order
-    static RGBA32FVec4ImageSoAIterator begin(const RGBAImageSoA &src)
+    static RGBA32FVecImageSoAIterator begin(const RGBAImageSoA &src)
     {
-        RGBA32FVec4ImageSoAIterator it;
-        it.m_r=reinterpret_cast<__m128*>(src.GetDataPointer<RGBAImageSoA::R>());
-        it.m_g=reinterpret_cast<__m128*>(src.GetDataPointer<RGBAImageSoA::G>());
-        it.m_b=reinterpret_cast<__m128*>(src.GetDataPointer<RGBAImageSoA::B>());
-        it.m_a=reinterpret_cast<__m128*>(src.GetDataPointer<RGBAImageSoA::A>());
+        typedef typename RGBA32FVec_traits<N>::pointer_type vptr_t;
+        RGBA32FVecImageSoAIterator it;
+        it.m_r=reinterpret_cast<vptr_t>(src.GetDataPointer<RGBAImageSoA::R>());
+        it.m_g=reinterpret_cast<vptr_t>(src.GetDataPointer<RGBAImageSoA::G>());
+        it.m_b=reinterpret_cast<vptr_t>(src.GetDataPointer<RGBAImageSoA::B>());
+        it.m_a=reinterpret_cast<vptr_t>(src.GetDataPointer<RGBAImageSoA::A>());
         it.m_offset = 0;
         return it;
     }
@@ -552,30 +563,46 @@ public:
     // Create an iterator at the end of the image. Note that for this to work
     // the image must have a multiple of 4 number of pixels or have allocated
     // additional elements to avoid segfaults.
-    static RGBA32FVec4ImageSoAIterator end(const RGBAImageSoA &src)
+    static RGBA32FVecImageSoAIterator end(const RGBAImageSoA &src)
     {
-        RGBA32FVec4ImageSoAIterator it = begin(src);
-        it.m_offset = (src.Size() + 3) >> 2;
+        RGBA32FVecImageSoAIterator it = begin(src);
+        it.m_offset = (src.Size() + (N-1)) / N;
         return it;
     }
 
 private:
 #ifndef NDEBUG
-    inline bool haveSameBase(const RGBA32FVec4ImageSoAIterator& other) const {
+    inline bool haveSameBase(const RGBA32FVecImageSoAIterator& other) const {
         return m_r == other.m_r && m_g == other.m_g &&
                m_b == other.m_b && m_a == other.m_a;
     }
 #endif
 
     // Pointers to the *base* data
-    __m128* m_r;
-    __m128* m_g;
-    __m128* m_b;
-    __m128* m_a;
+    typename RGBA32FVec_traits<N>::pointer_type m_r;
+    typename RGBA32FVec_traits<N>::pointer_type m_g;
+    typename RGBA32FVec_traits<N>::pointer_type m_b;
+    typename RGBA32FVec_traits<N>::pointer_type m_a;
     
     // Offset
     ptrdiff_t m_offset;
 };
+
+
+
+// RGBA SoA Pixel Iterator concept for SoA images. It iterates
+// the image in groups of 4 pixels, returning a fresh RGBA32FVec4 with the
+// next 4 R,G,B,A values in SoA form. Thus this is only a "read-only" iterator.
+// The current implementation only iterates in the original scanlie order of
+// an image.
+typedef RGBA32FVecImageSoAIterator<4> RGBA32FVec4ImageSoAIterator;
+
+// RGBA SoA Pixel Iterator concept for SoA images. It iterates
+// the image in groups of 8 pixels, returning a fresh RGBA32FVec8 with the
+// next 8 R,G,B,A values in SoA form. Thus this is only a "read-only" iterator.
+// The current implementation only iterates in the original scanlie order of
+// an image.
+typedef RGBA32FVecImageSoAIterator<8> RGBA32FVec8ImageSoAIterator;
 
 }
 
