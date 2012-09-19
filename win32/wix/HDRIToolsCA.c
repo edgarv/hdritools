ad0681786fdb87ba4e6ca017d546a82e3965c509
Edgar Velazquez-Armendariz <edgarv@users.noreply.github.com>
2012-09-19 16:58:12 -0400

    Initial work on a custom WiX function to check for AVX support.

--- win32/wix/HDRIToolsCA.c
+++ win32/wix/HDRIToolsCA.c
@@ -5,9 +5,32 @@
 #include <windows.h>
 #include <Shlobj.h>
 #include <msi.h>
+#include <intrin.h>
 
 UINT __stdcall NotifyAssocChanged (MSIHANDLE handle)
 {
     SHChangeNotify(SHCNE_ASSOCCHANGED, SHCNF_IDLIST, NULL, NULL);
     return ERROR_SUCCESS;    
+}
+
+UINT __stdcall CheckHasAVX (MSIHANDLE handle)
+{
+    BOOL avxSupported, hasOSXSAVE, hasAVX;
+    unsigned __int64 xcrFeatureMask;
+    int cpuInfo[4];
+
+    avxSupported = FALSE;
+    /* Visual Studio 2010 SP1 or later */
+#if (_MSC_FULL_VER >= 160040219)
+    /* AVX requires XSAVE/XSTORE, AVX flag and support from the OS */
+    __cpuid(cpuInfo, 1);
+    hasOSXSAVE = (cpuInfo[2] & (1 << 27)) != 0;
+    hasAVX     = (cpuInfo[2] & (1 << 28)) != 0;
+    if (hasOSXSAVE && hasAVX) {
+        /* Check if the OS is saving the AVX registers */
+        xcrFeatureMask = _xgetbv(_XCR_XFEATURE_ENABLED_MASK);
+        avxSupported = (xcrFeatureMask & 0x6) == 0x6;
+    }
+#endif
+    return avxSupported ? ERROR_SUCCESS : ERROR_INSTALL_FAILURE;
 }
