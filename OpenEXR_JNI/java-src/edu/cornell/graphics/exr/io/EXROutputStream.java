aba94c05fe796980ba59bd3cd86835f8073d1b76
Edgar Velazquez-Armendariz <edgarv@users.noreply.github.com>
2013-05-31 07:36:39 -0400

    Initial classes for generic file output

---
+++ OpenEXR_JNI/java-src/edu/cornell/graphics/exr/io/EXROutputStream.java
@@ -0,0 +1,62 @@
+/*============================================================================
+  HDRITools - High Dynamic Range Image Tools
+  Copyright 2008-2013 Program of Computer Graphics, Cornell University
+
+  Distributed under the OSI-approved MIT License (the "License");
+  see accompanying file LICENSE for details.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even the
+  implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  See the License for more information.
+ -----------------------------------------------------------------------------
+ Primary author:
+     Edgar Velazquez-Armendariz <cs#cornell#edu - eva5>
+============================================================================*/
+
+package edu.cornell.graphics.exr.io;
+
+import edu.cornell.graphics.exr.EXRIOException;
+import java.nio.ByteBuffer;
+
+/**
+ * Low-level EXR output stream interface.
+ * 
+ * @since 0.3
+ */
+public interface EXROutputStream {
+    
+    /**
+     * Write to the stream.
+     * 
+     * <p>{@code write(dst)} writes <em>n</em> bytes to the stream, where
+     * <em>n</em> is the number of bytes remaining in the buffer, that is,
+     * {@code dst.remaining()}, at the moment this method is invoked. The bytes
+     * are written from the current position of the buffer.</p>
+     * 
+     * <p>If an I/O error occurs, {@code write(dst)} throws an exception.</p>
+     * 
+     * @param dst The buffer from which bytes are to be transferred 
+     * @throws EXRIOException if an I/O error occurs.
+     */
+    void write(ByteBuffer dst) throws EXRIOException;
+   
+    /**
+     * Get the current writing position, in bytes from the beginning of the
+     * stream. If the next call to {@code write()} will write the first byte in
+     * the stream, {@code position()} returns {@literal 0}.
+     * 
+     * @return the current writing position, in bytes from the beginning of the
+     *         stream.
+     * @throws EXRIOException if an I/O error occurs.
+     */
+    long position() throws EXRIOException;
+    
+    /**
+     * Set the current position. After calling {@code position(i)},
+     * {@code position()} returns <em>i</em>.
+     * 
+     * @param pos the new writing position.
+     * @return a reference to this stream
+     */
+    EXROutputStream position(long pos) throws EXRIOException;
+}
---
+++ OpenEXR_JNI/java-src/edu/cornell/graphics/exr/io/XdrOutput.java
@@ -0,0 +1,226 @@
+/*============================================================================
+  HDRITools - High Dynamic Range Image Tools
+  Copyright 2008-2013 Program of Computer Graphics, Cornell University
+
+  Distributed under the OSI-approved MIT License (the "License");
+  see accompanying file LICENSE for details.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even the
+  implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  See the License for more information.
+ ----------------------------------------------------------------------------- 
+ Primary author:
+     Edgar Velazquez-Armendariz <cs#cornell#edu - eva5>
+============================================================================*/
+
+package edu.cornell.graphics.exr.io;
+
+import edu.cornell.graphics.exr.EXRIOException;
+import edu.cornell.graphics.exr.Half;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.charset.Charset;
+
+/**
+ * Helper class which encapsulates an {@code EXROutputStream} to write primitive
+ * and string data.
+ * 
+ * <p>Each instance uses a private buffer to write data into the stream. All
+ * methods are <em>not</em> thread safe, and assume that the stream will remain
+ * valid through the lifetime of a {@code XdrOutput} instance.</p>
+ * 
+ * <p>Modeled after the functions in ImfXdr.h from the C++ OpenEXR library.</p>
+ * 
+ * @see EXROutputStream
+ * @since 0.3
+ */
+public class XdrOutput {
+    
+    private final static int BUFFER_LEN = 512;
+    
+    /** Actual instance of the UTF8 charset to encode/decode strings */
+    private final Charset UTF8 = Charset.forName("UTF-8");
+    
+    private final ByteBuffer buffer;
+    private final EXROutputStream os;
+    
+    public XdrOutput(EXROutputStream stream) {
+        if (stream == null) {
+            throw new IllegalArgumentException("null input stream");
+        }
+        buffer = ByteBuffer.allocate(BUFFER_LEN);
+        if (!buffer.hasArray()) {
+            throw new IllegalStateException("buffer not backed by an array");
+        }
+        buffer.order(ByteOrder.LITTLE_ENDIAN);
+        os = stream;
+    }
+    
+    /**
+     * Get the current writing position, in bytes from the beginning of the
+     * stream. If the next call to {@code write()} will write the first byte in
+     * the stream, {@code position()} returns {@literal 0}.
+     * 
+     * <p>This implementation simply relies on
+     * {@link EXROutputStream#position() }.</p>
+     * 
+     * @return the current writing position, in bytes from the beginning of the
+     *         stream.
+     * @throws EXRIOException if an I/O error occurs.
+     */
+    public long position() throws EXRIOException {
+        return os.position();
+    }
+    
+    /**
+     * Set the current position. After calling {@code position(i)},
+     * {@code position()} returns <em>i</em>.
+     * 
+     * <p>This implementation simply relies on
+     * {@link EXROutputStream#position(long) }.</p>
+     * 
+     * @param pos the new writing position.
+     * @return a reference to this {@code XdrOutput} instance
+     */
+    public XdrOutput position(long pos) throws EXRIOException {
+        os.position(pos);
+        return this;
+    }
+    
+    public void writeBoolean(boolean value) throws EXRIOException {
+        writeByte(value ? (byte) 1 : 0);
+    }
+    
+    public void writeByte(byte b) throws EXRIOException {
+        buffer.rewind();
+        buffer.put(b);
+        buffer.flip();
+        os.write(buffer);
+    }
+    
+    public void writeUnsignedByte(int value) throws EXRIOException {
+        buffer.rewind();
+        buffer.put((byte) (value & 0xff));
+        buffer.flip();
+        os.write(buffer);
+    }
+    
+    public void writeShort(short value) throws EXRIOException {
+        buffer.rewind();
+        buffer.putShort(value);
+        buffer.flip();
+        os.write(buffer);
+    }
+    
+    public void writeUnsignedShort(int value) throws EXRIOException {
+        buffer.rewind();
+        buffer.putShort((short) (value & 0xffff));
+        buffer.flip();
+        os.write(buffer);
+    }
+    
+    public void writeInt(int value) throws EXRIOException {
+        buffer.rewind();
+        buffer.putInt(value);
+        buffer.flip();
+        os.write(buffer);
+    }
+    
+    public void writeUnsignedInt(long value) throws EXRIOException {
+        buffer.rewind();
+        buffer.putInt((int) (value & 0xffffffffL));
+        buffer.flip();
+        os.write(buffer);
+    }
+    
+    public void writeFloat(float value) throws EXRIOException {
+        buffer.rewind();
+        buffer.putFloat(value);
+        buffer.flip();
+        os.write(buffer);
+    }
+    
+    public void writeDouble(double value) throws EXRIOException {
+        buffer.rewind();
+        buffer.putDouble(value);
+        buffer.flip();
+        os.write(buffer);
+    }
+    
+    public void writeHalf(Half value) throws EXRIOException {
+        final short s = value.halfToRawShortBits();
+        writeShort(s);
+    }
+    
+    public void writeNullTerminatedUTF8(String s) throws EXRIOException {
+        final byte[] b = s.getBytes(UTF8);
+        writeByteArray(b);
+        writeByte((byte) 0);
+    }
+    
+    public void writeUTF8(String s) throws EXRIOException {
+        final byte[] b = s.getBytes(UTF8);
+        writeByteArray(b);
+    }
+    
+    /**
+     * Add {@code n} padding bytes. The added bytes are set to zero.
+     * If {@code n} is less than one the function returns immediately.
+     * 
+     * @param n number of padding bytes to add.
+     */
+    public void pad(int n) throws EXRIOException {
+        if (n < 1) {
+            return;
+        }
+        int count = 0;
+        while (count < n) {
+            buffer.rewind();
+            final int limit = Math.min(n - count, BUFFER_LEN);
+            for (int i = 0; i < limit; ++i, ++count) {
+                buffer.put((byte) 0);
+            }
+            buffer.flip();
+            os.write(buffer);
+        }
+        assert count == n;
+    }
+    
+    public void writeByteArray(byte[] b) throws EXRIOException {
+        writeByteArray(b, 0,  b.length);
+    }
+    
+    /**
+     * 
+     * @param b
+     * @param off
+     * @param len
+     * @throws EXRIOException
+     * @throws IllegalArgumentException if {@code b} is null.
+     * @throws IndexOutOfBoundsException if {@code off} is negative,
+     *         {@code len} is negative, or {@code len} is greater than
+     *         {@code b.length - off}
+     */
+    public void writeByteArray(byte[] b,int off,int len) throws EXRIOException {
+        if (b == null) {
+            throw new IllegalArgumentException("null destination buffer");
+        } else if (off < 0) {
+            throw new IndexOutOfBoundsException("invalid offset: " + off);
+        } else if (len < 0 || len > (b.length - off)) {
+            throw new IndexOutOfBoundsException("invalid length: " + len);
+        }
+        
+        assert buffer.array() != null;
+        final int numPasses = (len + BUFFER_LEN - 1) / BUFFER_LEN;
+        assert BUFFER_LEN*numPasses >= len;
+        for (int passIdx = 0; passIdx < numPasses; ++passIdx) {
+            final int length = Math.min(BUFFER_LEN, len - BUFFER_LEN*passIdx);
+            // This should be more efficient than buffer.put(b, offset, length)
+            System.arraycopy(b, BUFFER_LEN*passIdx, buffer.array(), 0, length);
+            buffer.limit(length).rewind();
+            os.write(buffer);
+            buffer.put(b, off, length);
+        }
+    }
+    
+}
