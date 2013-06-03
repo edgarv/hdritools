06f6ffe2b8e37f890be512943aeca8e34d2da805
Edgar Velazquez-Armendariz <edgarv@users.noreply.github.com>
2013-06-03 02:43:57 -0400

    Variation on the intial test of EXROutputImage which uses EXRFileOutputStream

---
+++ OpenEXR_JNI/java_test-src/edu/cornell/graphics/exr/EXROutputFileBasicTest2.java
@@ -0,0 +1,36 @@
+/*============================================================================
+  HDRITools - High Dynamic Range Image Tools
+  Copyright 2008-2013 Program of Computer Graphics, Cornell University
+
+  Distributed under the OSI-approved MIT License (the "License");
+  see accompanying file LICENSE for details.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even the
+  implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  See the License for more information.
+ -----------------------------------------------------------------------------
+ Primary author:
+     Edgar Velazquez-Armendariz <cs#cornell#edu - eva5>
+============================================================================*/
+
+package edu.cornell.graphics.exr;
+
+import edu.cornell.graphics.exr.io.EXRFileOutputStream;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Initial test suite for EXROutputFile using Java {@code EXROutputStream}
+ * instances from the native code.
+ */
+public class EXROutputFileBasicTest2 extends EXROutputFileBasicTest1 {
+
+    @Override
+    protected EXROutputFile createOutputFile(File file, Header header) 
+            throws IOException {
+        EXROutputFile output = new EXROutputFile(
+                new EXRFileOutputStream(file.toPath()), header, true);
+        return output;
+    }
+    
+}
