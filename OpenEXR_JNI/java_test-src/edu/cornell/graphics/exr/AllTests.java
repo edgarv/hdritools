8b636cdf0e37fab720e7b948698d6617b55f7080
Edgar Velazquez-Armendariz <edgarv@users.noreply.github.com>
2013-06-07 15:11:02 -0400

    Add the EXROutputFile test cases to the general suite

--- OpenEXR_JNI/java_test-src/edu/cornell/graphics/exr/AllTests.java
+++ OpenEXR_JNI/java_test-src/edu/cornell/graphics/exr/AllTests.java
@@ -10,7 +10,9 @@ import org.junit.runners.Suite;
     edu.cornell.graphics.exr.io.InputFileInfoBasicTest.class,
     edu.cornell.graphics.exr.HeaderBasicTest.class,
     edu.cornell.graphics.exr.PixelTypeTest.class,
-    edu.cornell.graphics.exr.EXRInputFileBasicTest.class
+    edu.cornell.graphics.exr.EXRInputFileBasicTest.class,
+    edu.cornell.graphics.exr.EXROutputFileBasicTest1.class,
+    edu.cornell.graphics.exr.EXROutputFileBasicTest2.class
 })
 public class AllTests {
     // Nothing to be done
