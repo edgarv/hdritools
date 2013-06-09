e49b64b9c49b4b0ad000d2b2d11cbde660a8db63
Edgar Velazquez-Armendariz <edgarv@users.noreply.github.com>
2013-06-08 22:38:19 -0400

    Set the number of threads to Runtime.availableProcessors() upon JNI loading

--- OpenEXR_JNI/jniBinding.cpp
+++ OpenEXR_JNI/jniBinding.cpp
@@ -14,6 +14,9 @@
 ============================================================================*/
 
 #include <jni.h>
+#include "util.h"
+
+#include <ImfThreading.h>
 
 
 
@@ -21,12 +24,29 @@
 jint JNICALL JNI_OnLoad(JavaVM *vm, void *reserved)
 {
     union {
-        JNIEnv* env;
+        JNIEnv* envPtr;
         void* void_env;
     };
     if (vm->GetEnv(&void_env, JNI_VERSION_1_6) != JNI_OK) {
         return -1;
     }
+
+    try {
+        JNIEnvHelper env(envPtr);
+        jclass clazz = env.findClassLocalRef("java/lang/Runtime");
+        jmethodID getRuntimeID = env.getStaticMethodID(clazz,
+            "getRuntime", "()Ljava/lang/Runtime;");
+        jmethodID availableProcessorsID = env.getMethodID(clazz,
+            "availableProcessors", "()I");
+        jobject runtime = envPtr->CallStaticObjectMethod(clazz, getRuntimeID);
+        if (runtime != NULL) {
+            int count = envPtr->CallIntMethod(runtime, availableProcessorsID);
+            Imf::setGlobalThreadCount(count);
+        }
+    } catch (...) {
+        // nothing to do
+    }
+
     return JNI_VERSION_1_6;
 }
 
