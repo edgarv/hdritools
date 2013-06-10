82bfd34b81c6efc4b4bc034d69b750c9fdcda8ee
Edgar Velazquez-Armendariz <edgarv@users.noreply.github.com>
2013-06-10 13:05:09 -0400

    Added documentation to Chromaticities; the default constructor sets sRGB primaries

--- OpenEXR_JNI/java-src/edu/cornell/graphics/exr/Chromaticities.java
+++ OpenEXR_JNI/java-src/edu/cornell/graphics/exr/Chromaticities.java
@@ -15,15 +15,51 @@
 
 package edu.cornell.graphics.exr;
 
-// TODO: Add documentation
+/**
+ * The CIE {@code x} and {@code y} coordinates of the RGB triplets
+ * (1,0,0), (0,1,0), (0,0,1) and (1,1,1).
+ * 
+ * @since OpenEXR-JNI 2.1
+ */
 public class Chromaticities {
-    public float redX,   redY;
-    public float greenX, greenY;
-    public float blueX,  blueY;
-    public float whiteX, whiteY;
+    /** CIE {@code x} coordinate of the RGB triplet (1,0,0). */
+    public float redX;
+    /** CIE {@code y} coordinate of the RGB triplet (1,0,0). */
+    public float redY;
+    /** CIE {@code x} coordinate of the RGB triplet (0,1,0). */
+    public float greenX;
+    /** CIE {@code y} coordinate of the RGB triplet (0,1,0). */
+    public float greenY;
+    /** CIE {@code x} coordinate of the RGB triplet (0,0,1). */
+    public float blueX;
+    /** CIE {@code y} coordinate of the RGB triplet (0,0,1). */
+    public float blueY;
+    /** CIE {@code x} coordinate of the RGB triplet (1,1,1). */
+    public float whiteX;
+    /** CIE {@code y} coordinate of the RGB triplet (1,1,1). */
+    public float whiteY;
     
-    public Chromaticities() {}
+    /**
+     * Construct a new {@code Chromaticities} instance according to the
+     * Rec. ITU-R BT.709-3 primaries.
+     */
+    public Chromaticities() {
+        this.redX   = 0.6400f;
+        this.redY   = 0.3300f;
+        this.greenX = 0.3000f;
+        this.greenY = 0.6000f;
+        this.blueX  = 0.1500f;
+        this.blueY  = 0.0600f;
+        this.whiteX = 0.3127f;
+        this.whiteY = 0.3290f;
+    }
     
+    /**
+     * Constructs a new {@code Chromaticities} instance with the same
+     * CIE coordinates as {@code other}.
+     * 
+     * @param other a non-null {@code Chromaticities} object
+     */
     public Chromaticities(Chromaticities other) {
         this.redX   = other.redX;
         this.redY   = other.redY;
@@ -35,6 +71,19 @@ public class Chromaticities {
         this.whiteY = other.whiteY;
     }
 
+    /**
+     * Constructs a new {@code Chromaticities}instance with specific
+     * CIE coordinates.
+     * 
+     * @param redX   CIE {@code x} coordinate of the RGB triplet (1,0,0)
+     * @param redY   CIE {@code y} coordinate of the RGB triplet (1,0,0)
+     * @param greenX CIE {@code x} coordinate of the RGB triplet (0,1,0)
+     * @param greenY CIE {@code y} coordinate of the RGB triplet (0,1,0)
+     * @param blueX  CIE {@code x} coordinate of the RGB triplet (0,0,1)
+     * @param blueY  CIE {@code y} coordinate of the RGB triplet (0,0,1)
+     * @param whiteX CIE {@code x} coordinate of the RGB triplet (1,1,1)
+     * @param whiteY CIE {@code y} coordinate of the RGB triplet (1,1,1)
+     */
     public Chromaticities(float redX, float redY, float greenX, float greenY,
             float blueX, float blueY, float whiteX, float whiteY) {
         this.redX   = redX;
@@ -47,6 +96,16 @@ public class Chromaticities {
         this.whiteY = whiteY;
     }
 
+    /**
+     * Compares this chromaticities instance to the specified object. The result
+     * is {@code true} if and only if the argument is not {@code null}, is a
+     * {@code Chromaticities} object and all their CIE coordinates are the same.
+     *
+     * @param obj The object to compare this instance against
+     * @return {@code true} if the given object represents a
+     *         {@code Chromaticities} equivalent to this instance,
+     *         {@code false} otherwise
+     */
     @Override
     public boolean equals(Object obj) {
         if (obj == null) {
@@ -83,6 +142,12 @@ public class Chromaticities {
         return true;
     }
 
+    /**
+     * Returns a hash code for this chromaticities instance. The hash code
+     * is computes by an aggregate of each of its CIE coordinates.
+     * 
+     * @return a hash code for this chromaticities instance
+     */
     @Override
     public int hashCode() {
         int hash = 7;
@@ -97,6 +162,10 @@ public class Chromaticities {
         return hash;
     }
 
+    /**
+     * Returns a string representation of the chromaticities.
+     * @return a string representation of the chromaticities
+     */
     @Override
     public String toString() {
         return "Chromaticities{" +
