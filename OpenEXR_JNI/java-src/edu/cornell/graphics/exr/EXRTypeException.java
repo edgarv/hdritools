b7625ee0f5e521845d70a389a8034300a7212f9c
Edgar Velazquez-Armendariz <edgarv@users.noreply.github.com>
2013-06-10 10:49:29 -0400

    Improved exception documentation; added missing serialVersionUID

--- OpenEXR_JNI/java-src/edu/cornell/graphics/exr/EXRTypeException.java
+++ OpenEXR_JNI/java-src/edu/cornell/graphics/exr/EXRTypeException.java
@@ -15,22 +15,73 @@
 
 package edu.cornell.graphics.exr;
 
-// TODO: Add documentation
-public class EXRTypeException extends RuntimeException {
+import edu.cornell.graphics.exr.attributes.Attribute;
 
+/**
+ * Signals an error when handling typed attributes in an OpenEXR file header.
+ * 
+ * @see Header#insert(String, Attribute) 
+ * @see Header#getTypedAttribute(String, Class) 
+ * @since OpenEXR-JNI 2.1
+ */
+public class EXRTypeException extends RuntimeException {
+    private static final long serialVersionUID = -3925158591514088326L;
+
+    /** 
+     * Constructs a new {@code EXRTypeException} with the specified cause and a
+     * detail message of {@code (cause==null ? null : cause.toString())}
+     * (which typically contains the class and detail message of
+     * {@code cause}.)  This constructor is useful if the
+     * {@code EXRTypeException} being thrown is little more than a wrapper for
+     * other throwable.
+     *
+     * @param  cause the cause (which is saved for later retrieval by the
+     *         {@link #getCause()} method.)  (A {@code null} value is
+     *         permitted, and indicates that the cause is nonexistent or
+     *         unknown.)
+     */
     public EXRTypeException(Throwable cause) {
         super(cause);
     }
 
+    /**
+     * Constructs a new {@code EXRTypeException} with the specified 
+     * detail message and cause.
+     * 
+     * <p>Note that the detail message associated with
+     * {@code cause} is <em>not</em> automatically incorporated in
+     * this exception's detail message.</p>
+     *
+     * @param  message the detail message (which is saved for later retrieval
+     *         by the {@link #getMessage()} method.)
+     * @param  cause the cause (which is saved for later retrieval by the
+     *         {@link #getCause()} method.)  (A {@code null} value is
+     *         permitted, and indicates that the cause is nonexistent or
+     *         unknown.)
+     */
     public EXRTypeException(String message, Throwable cause) {
         super(message, cause);
     }
 
+    /**
+     * Constructs a {@code EXRTypeException} with the specified
+     * detail message. The cause is not initialized, and may subsequently be
+     * initialized by a call to {@link #initCause}.
+     *
+     * @param   message   the detail message. The detail message is saved for
+     *          later retrieval by the {@link #getMessage()} method.
+     */
     public EXRTypeException(String message) {
         super(message);
     }
 
+    /** 
+     * Constructs a new {@code EXRTypeException} with {@code null} as its
+     * detail message.  The cause is not initialized, and may subsequently be
+     * initialized by a call to {@link #initCause}.
+     */
     public EXRTypeException() {
+        super();
     }
     
 }
--- OpenEXR_JNI/java-src/edu/cornell/graphics/exr/NoSuchChannelException.java
+++ OpenEXR_JNI/java-src/edu/cornell/graphics/exr/NoSuchChannelException.java
@@ -22,18 +22,60 @@ package edu.cornell.graphics.exr;
 class NoSuchChannelException extends RuntimeException {
     private static final long serialVersionUID = -7878358869230043028L;
 
+    /** 
+     * Constructs a new {@code NoSuchChannelException} with the specified cause
+     * and a detail message of {@code (cause==null ? null : cause.toString())}
+     * (which typically contains the class and detail message of
+     * {@code cause}.)  This constructor is useful if the
+     * {@code NoSuchChannelException} being thrown is little more than a wrapper
+     * for other throwable.
+     *
+     * @param  cause the cause (which is saved for later retrieval by the
+     *         {@link #getCause()} method).  (A {@code null} value is
+     *         permitted, and indicates that the cause is nonexistent or
+     *         unknown.)
+     */
     public NoSuchChannelException(Throwable cause) {
         super(cause);
     }
 
+    /**
+     * Constructs a new {@code NoSuchChannelException} with the specified 
+     * detail message and cause.
+     * 
+     * <p>Note that the detail message associated with
+     * {@code cause} is <em>not</em> automatically incorporated in
+     * this exception's detail message.</p>
+     *
+     * @param  message the detail message (which is saved for later retrieval
+     *         by the {@link #getMessage()} method.)
+     * @param  cause the cause (which is saved for later retrieval by the
+     *         {@link #getCause()} method.)  (A {@code null} value is
+     *         permitted, and indicates that the cause is nonexistent or
+     *         unknown.)
+     */
     public NoSuchChannelException(String message, Throwable cause) {
         super(message, cause);
     }
 
+    /**
+     * Constructs a {@code NoSuchChannelException} with the specified
+     * detail message. The cause is not initialized, and may subsequently be
+     * initialized by a call to {@link #initCause}.
+     *
+     * @param   message   the detail message. The detail message is saved for
+     *          later retrieval by the {@link #getMessage()} method.
+     */
     public NoSuchChannelException(String message) {
         super(message);
     }
 
+    /** 
+     * Constructs a new {@code NoSuchChannelException} with {@code null} as its
+     * detail message.  The cause is not initialized, and may subsequently be
+     * initialized by a call to {@link #initCause}.
+     */
     public NoSuchChannelException() {
+        super();
     }
 }
