0a3f133db22b6023bd84e5a93d7c8c3ce82ec1f0
Edgar Velazquez-Armendariz <edgarv@users.noreply.github.com>
2013-06-10 13:28:53 -0400

    Added documentation for the LineOrder enum

--- OpenEXR_JNI/java-src/edu/cornell/graphics/exr/LineOrder.java
+++ OpenEXR_JNI/java-src/edu/cornell/graphics/exr/LineOrder.java
@@ -15,9 +15,19 @@
 
 package edu.cornell.graphics.exr;
 
-// TODO: Add documentation
+/**
+ * Layout of the pixel data in an OpenEXR image file.
+ * 
+ * @since OpenEXR-JNI 2.1
+ */
 public enum LineOrder {
+    /**
+     * First scan line has lowest y coordinate. */
     INCREASING_Y, // 0
+    
+    /** First scan line has highest y coordinate. */
     DECREASING_Y, // 1
+    
+    /** Only for tiled files; tiles are written in random order. */
     RANDOM_Y // 2
 }
