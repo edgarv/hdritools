b243bd4093f43a709051d84c28a6c0d2bb79cf16
Edgar Velazquez-Armendariz <edgarv@users.noreply.github.com>
2014-05-05 19:46:46 -0400

    Improved Matlab test case, it can run from the command line on Unix

--- OpenEXR_Matlab/test/launch_test.m.cmake
+++ OpenEXR_Matlab/test/launch_test.m.cmake
@@ -1,11 +1,16 @@
 % Simple bootstraper for the tests
 
 function launch_test(mexpath)
-    fprintf('OpenEXR MEX test\n')
+    disp('OpenEXR MEX test')
     fprintf('Adding to the path \"%s\"\n', mexpath)
     addpath(mexpath)
     addpath('@test_dir@')
     tic
-    @test_script@
+    try
+      @test_script@
+    catch ME
+      fprintf(2, getReport(ME))
+    end
     toc
+    exit
 end
--- OpenEXR_Matlab/test/x35_3a_test.m
+++ OpenEXR_Matlab/test/x35_3a_test.m
@@ -10,68 +10,40 @@ fname = 'x35_3a.exr';
 
 finfo = exrinfo(fname);
 
-if ~all(size(finfo.channels) == [1 49])
-   fprintf(2, 'Unexpected number of channels!\n');
-   return
-end
+assert(all(size(finfo.channels)==[1 49]), 'Unexpected number of channels!')
 
 % Test some channels
 cExpected = {'Background.G' 'Caustics.G' 'Diffuse.B' 'Light.R'};
 cActual   = {finfo.channels{7} finfo.channels{10} ...
     finfo.channels{12} finfo.channels{21}};
-if ~all(strcmp(cExpected, cActual))
-   fprintf(2, 'Unexpected channel names!\n');
-   return
-end
+assert(all(strcmp(cExpected, cActual)), 'Unexpected channel names!');
 
 % Image size
-if ~all(finfo.size == [384 547])
-   fprintf(2, 'Unexpected image size!\n');
-   return
-end
+assert(all(finfo.size == [384 547]), 'Unexpected image size!');
 
 % Check some attributes
 attr = finfo.attributes;
-if ~strcmp(attr('compression'), 'zips')
-   fprintf(2, 'Unexpected compression!\n');
-   return
-end
+assert(strcmp(attr('compression'), 'zips'), 'Unexpected compression!');
 dw=attr('dataWindow');
-if ~all([dw.min dw.max] == [70 0 616 383])
-   fprintf(2, 'Unexpected data window!\n');
-   return 
-end
+assert(all([dw.min dw.max] == [70 0 616 383]), 'Unexpected data window!');
 
 
 % Check reading some channels through both interfaces
 imgRGB = exrread(fname);
 [iR, iG] = exrreadchannels(fname, 'R', 'G');
 iB = exrreadchannels(fname, 'B');
-if ~all(all(imgRGB(:,:,1) == iR))
-   fprintf(2, 'Unexpected R channel!\n');
-   return 
-end
-if ~all(all(imgRGB(:,:,2) == iG))
-   fprintf(2, 'Unexpected G channel!\n');
-   return 
-end
-if ~all(all(imgRGB(:,:,3) == iB))
-   fprintf(2, 'Unexpected B channel!\n');
-   return 
-end
+assert(all(all(imgRGB(:,:,1) == iR)), 'Unexpected R channel!');
+assert(all(all(imgRGB(:,:,2) == iG)), 'Unexpected G channel!');
+assert(all(all(imgRGB(:,:,3) == iB)), 'Unexpected B channel!');
 
 cNames = {'Atmosphere.G' 'Velocity.Y' 'realcolor.B' 'RawGI.R' 'depth.Z'};
 cData = exrreadchannels(fname, cNames);
 vyData = exrreadchannels(fname, 'Velocity.Y');
-if ~all(all(vyData == cData('Velocity.Y')))
-   fprintf(2, 'Unexpected Velocity.Y channel data!\n');
-   return
-end
+assert(all(all(vyData == cData('Velocity.Y'))), ...
+    'Unexpected Velocity.Y channel data!');
 
-if int32(sum(sum(cData('RawGI.R')))) ~= 15970
-   fprintf(2, 'Unexpected RawGI.R channel data!\n');
-   return   
-end
+assert(int32(sum(sum(cData('RawGI.R')))) == 15970, ...
+    'Unexpected RawGI.R channel data!');
 
 % Assemble an image with some channels
 giChannels = {'RawGI.R' 'RawGI.G' 'RawGI.B'};
@@ -80,11 +52,14 @@ giImg(:,:,1) = giR;
 giImg(:,:,2) = giG;
 giImg(:,:,3) = giB;
 
+f = figure('Name', 'OpenEXR test, close figure to continue');
 subplot(2,2,1); imshow(imgRGB); title('RGB');
 subplot(2,2,2); imshow(giImg); title('RawGI Layer');
 subplot(2,2,3); imagesc(cData('Velocity.Y')); axis image; axis off; title('Velocity.Y'); colorbar;
 subplot(2,2,4); imagesc(cData('depth.Z')); axis image; axis off; title('depth.Z'); colorbar;
+drawnow     % Necessary to print the message
+waitfor(f);
 
 
 
-fprintf('All tests were successful.\n')
+disp('All OpenEXR tests were successful.')
